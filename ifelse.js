f9c8244937f9eb109b309e5972a02c451ed13b51
nainagoyal361 <nainagoyal361@gmail.com>
2026-03-09 09:18:50 +0530

    First Commit

---
+++ ifelse.js
@@ -0,0 +1,13 @@
+let marks=70;
+if(marks>=90){
+    console.log("Grade A");
+}
+else if(marks>=80 && marks<90 ){
+    console.log("Grade B")
+}
+else if(marks>=70 && marks<80){
+    console.log("Grade C")
+}
+else{
+    console.log("Grade D")
+}
---
+++ switch.js
@@ -0,0 +1,14 @@
+let day="Monday";
+switch(day){
+    case"Monday":
+    console.log("Start of the week")
+    break;
+    case"Tuesday":
+    console.log("Second day of the week")  
+    break;
+    case"Wednesday":
+    console.log("Third day of the week")
+    break;
+    
+
+}
